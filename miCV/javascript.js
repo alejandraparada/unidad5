fc06ad75fba5c9849c75c425c33035abeeb5c5df
alejandraparada <carla.parada.aguilera@gmail.com>
2023-02-15 19:05:19 +0100

    ejercicio CV

---
+++ miCV/javascript.js
@@ -0,0 +1,20 @@
+function vermas(id){
+    if(id=="mas"){
+    document.getElementById("desplegar").style.display="block";   
+    document.getElementById("mas").style.display="none"; 
+    }
+    else{
+    document.getElementById("desplegar").style.display="none";
+    document.getElementById("mas").style.display="inline";
+    }
+}
+function vermas_Con(id){
+    if(id=="mas"){
+    document.getElementById("desplegar_con").style.display="block";   
+    document.getElementById("mas_con").style.display="none"; 
+    }
+    else{
+    document.getElementById("desplegar_con").style.display="none";
+    document.getElementById("mas_con").style.display="inline";
+    }
+}
